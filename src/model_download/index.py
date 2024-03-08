05c11df8ee6f9708cd1e0b6fe58f69029b8e9de7
zhonglun.wj <zhonglun.wj@alibaba-inc.com>
2024-03-08 14:15:56 +0800

    fix download func

--- src/model_download/index.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import snapshot_download
-import os
-
-
-def handler(event, context):
-    model = os.getenv("MODEL_ID")
-    print("start download %s!"%model)
-    snapshot_download(repo_id=model, revision="main")
-    print("finish download %s!"%model)
